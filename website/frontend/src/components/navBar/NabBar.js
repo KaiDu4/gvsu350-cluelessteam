aef72d79df341b8a211e1766bef30ac26a35bcec
ChotanSharma <chotansharma@yahoo.com>
2024-10-31 04:10:38 -0400

    Modify navBar component by adding LinkContainer

--- website/frontend/src/components/navBar/NabBar.js
+++ website/frontend/src/components/navBar/NabBar.js
@@ -1,20 +1,25 @@
 import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from "react-router-dom";
 import './NabBar.css'
+import { NavLink } from 'react-bootstrap';
 
-function NabBar() {
+function NabBar() { 
   return (
     <Navbar expand="lg" className="custom-navbar">
       <Container className='justify-content-between'>
         <div className='d-flex justify-content-center w-100'>
-        <Navbar.Brand href="#home" className='navbar-brand custom-font'>EduBridge</Navbar.Brand>
+        <Navbar.Brand className='navbar-brand custom-font'>EduBridge</Navbar.Brand>
         </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className='nav-link custom-font'>Home</Nav.Link>
+            <LinkContainer to="/">
+              <Nav.Link className='nav-link custom-font'>Home</Nav.Link>
+            </LinkContainer>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown" className='nav-dropdown custom-font'>
               <NavDropdown.Item href="#signin" className='dropdown-item custom-font'>START</NavDropdown.Item>
               <NavDropdown.Item href="#publicTemp" className='dropdown-item custom-font'>
